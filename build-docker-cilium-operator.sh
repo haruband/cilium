18eb90d9bc6701bde6374727c8b567a7c5b12058
haruband <haruband@gmail.com>
2021-04-01 10:37:57 +0900

    Fix build-docker-cilium-operator.sh

--- build-docker-cilium-operator.sh
+++ build-docker-cilium-operator.sh
@@ -5,4 +5,4 @@ DOCKER_DEV_ACCOUNT=${2:-haruband}
 
 DOCKER_DEV_ACCOUNT=docker.io/${DOCKER_DEV_ACCOUNT} DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG} make docker-operator-generic-image
 
-docker push docker.io/${DOCKER_DEV_ACCOUNT}/cilium-dev:${DOCKER_IMAGE_TAG}
+docker push docker.io/${DOCKER_DEV_ACCOUNT}/operator-generic:${DOCKER_IMAGE_TAG}
