9ac44409be0b3a046f71e016e3d554b0c7093412
haruband <haruband@gmail.com>
2021-04-14 14:33:18 +0000

    Fix update-bpf.sh

--- update-bpf.sh
+++ update-bpf.sh
@@ -5,4 +5,4 @@ OBJFILE=${2:-bpf_host.o}
 SECNAME=${3:-to-netdev}
 NETDEV=${4:-ens33}
 
-tc filter replace dev ${NETDEV} ${POLICY} prio 1 handle 1 bpf da obj ${OBJFILE} sec ${SECNAME} verbose
+sudo tc filter replace dev ${NETDEV} ${POLICY} prio 1 handle 1 bpf da obj ${OBJFILE} sec ${SECNAME} verbose
