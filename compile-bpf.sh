92beabc15702ce2b580a1f45bc9165ac519f4266
haruband <haruband@gmail.com>
2021-04-29 03:54:22 +0000

    Fix compile-bpf.sh

--- compile-bpf.sh
+++ compile-bpf.sh
@@ -13,5 +13,5 @@ NODE_MAC="{.addr=$(mac2array $NODE_MAC)}"
 
 clang -emit-llvm -g -O2 -target bpf -std=gnu89 -nostdinc -DNODE_MAC=${NODE_MAC} -DSECLABEL=12345678 -DFROM_ENCAP_DEV=1 -D__NR_CPUS__=128 -DENABLE_ARP_RESPONDER=1 -Wall -Wextra -Werror -Wshadow -Wno-address-of-packed-member -Wno-unknown-warning-option -Wno-gnu-variable-sized-type-not-at-end -Wdeclaration-after-statement -Iconfigs/${TARGET} -Iconfigs -Iinclude -I. -c bpf_${TARGET}.c -o bpf_${TARGET}.bc
 #opt -O2 -debug-pass=Arguments bpf_${TARGET}.ll -disable-output
-#opt -loop-simplify -S bpf_${TARGET}.bc > bpf_${TARGET}.ll
+opt -S bpf_${TARGET}.bc > bpf_${TARGET}.ll
 llc -mattr=dwarfris -march=bpf -mcpu=v2 -filetype=obj -o bpf_${TARGET}.o bpf_${TARGET}.ll
