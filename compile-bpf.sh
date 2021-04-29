af3566d5f953e60c1f4b48b9e68fbb5e2772aa4a
haruband <haruband@gmail.com>
2021-04-29 05:40:17 +0000

    Fix compile-bpf.sh

--- compile-bpf.sh
+++ compile-bpf.sh
@@ -2,7 +2,7 @@
 
 TARGET=${1:-host}
 
-clang -emit-llvm -O2 -target bpf -g -std=gnu89 -nostdinc -DNODE_MAC="{.addr={0xb3,0x05,0x00,0x00,0x00,0x00}}" -DSECLABEL=12345678 -D__NR_CPUS__=128 -DENABLE_ARP_RESPONDER=1 -Wall -Wextra -Werror -Wshadow -Wno-address-of-packed-member -Wno-unknown-warning-option -Wno-gnu-variable-sized-type-not-at-end -Wdeclaration-after-statement -I. -Iglobals -I/var/lib/cilium/bpf -I/var/lib/cilium/bpf/include -c /var/lib/cilium/bpf/bpf_${TARGET}.c -o bpf_${TARGET}.bc
-#opt -O2 -debug-pass=Arguments bpf_${TARGET}.ll -disable-output
+clang -emit-llvm -O1 -Rpass=gvn -target bpf -g -std=gnu89 -nostdinc -DNODE_MAC="{.addr={0xb3,0x05,0x00,0x00,0x00,0x00}}" -DSECLABEL=12345678 -D__NR_CPUS__=128 -DENABLE_ARP_RESPONDER=1 -Wall -Wextra -Werror -Wshadow -Wno-address-of-packed-member -Wno-unknown-warning-option -Wno-gnu-variable-sized-type-not-at-end -Wdeclaration-after-statement -I. -Iglobals -I/var/lib/cilium/bpf -I/var/lib/cilium/bpf/include -c /var/lib/cilium/bpf/bpf_${TARGET}.c -o bpf_${TARGET}.bc
 opt -S bpf_${TARGET}.bc > bpf_${TARGET}.ll
-llc -mattr=dwarfris -march=bpf -mcpu=v2 -filetype=obj -o bpf_${TARGET}.o bpf_${TARGET}.ll
+#opt -O2 -debug-pass=Arguments -S bpf_${TARGET}.bc -o bpf_${TARGET}.ll
+llc -mattr=dwarfris -march=bpf -mcpu=v2 -filetype=obj -o tmp_${TARGET}.o bpf_${TARGET}.ll
