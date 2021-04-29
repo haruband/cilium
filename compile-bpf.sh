48ad7aa5862af3ef727fd5fff8c2459a60f4580a
haruband <haruband@gmail.com>
2021-04-29 03:21:30 +0000

    Fix compile-bpf.sh

--- compile-bpf.sh
+++ compile-bpf.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 TARGET=${1:-host}
+NETDEV=${2:-cilium_vxlan}
 
-clang -emit-llvm -g -O1 -target bpf -std=gnu89 -nostdinc -D__NR_CPUS__=128 -Wall -Wextra -Werror -Wshadow -Wno-address-of-packed-member -Wno-unknown-warning-option -Wno-gnu-variable-sized-type-not-at-end -Wdeclaration-after-statement -Iconfigs/${TARGET} -Iconfigs -Iinclude -I. -c bpf_${TARGET}.c -o bpf_${TARGET}.bc
+function mac2array()
+{
+  echo "{0x${1//:/,0x}}"
+}
+
+NODE_MAC=$(ip link show ${NETDEV} | grep ether | awk '{print $2}')
+NODE_MAC="{.addr=$(mac2array $NODE_MAC)}"
+
+clang -emit-llvm -g -O2 -target bpf -std=gnu89 -nostdinc -DNODE_MAC=${NODE_MAC} -DSECLABEL=12345678 -DFROM_ENCAP_DEV=1 -D__NR_CPUS__=128 -DENABLE_ARP_RESPONDER=1 -Wall -Wextra -Werror -Wshadow -Wno-address-of-packed-member -Wno-unknown-warning-option -Wno-gnu-variable-sized-type-not-at-end -Wdeclaration-after-statement -Iconfigs/${TARGET} -Iconfigs -Iinclude -I. -c bpf_${TARGET}.c -o bpf_${TARGET}.bc
 #opt -O2 -debug-pass=Arguments bpf_${TARGET}.ll -disable-output
-opt -loop-simplify -S bpf_${TARGET}.bc > bpf_${TARGET}.ll
+#opt -loop-simplify -S bpf_${TARGET}.bc > bpf_${TARGET}.ll
 llc -mattr=dwarfris -march=bpf -mcpu=v2 -filetype=obj -o bpf_${TARGET}.o bpf_${TARGET}.ll
